949a2003ec86892cbe6424d08ef21741789dfc44
Popidan <cpopidan@gmail.com>
2022-05-30 19:40:00 +0300

    final product

--- login_form_view.php
+++ login_form_view.php
@@ -7,6 +7,14 @@
             <input type = "password" name="password" class = "form-control" style = "width : 250px" required><br>
             <input type = "submit" class="btn btn-default">
         </form>
+        <?php
+        if ($_GET['errorId']==1){
+            echo "<script>alert('this username does not exist')</script>";
+        }
+        elseif($_GET['errorId']==2){
+            echo "<script>alert('the password is wrong')</script>";
+        }
+        ?>
         <a href = "signup_page.php?errorId=0"  class="LogSig" style="text-align: -webkit-center">Sign Up</a>
     </div>
 </html>
